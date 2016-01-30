8f62f563639ee087536b2cb7fa42063cea6f4ff9
rpruciak <robert.pruciak@gmail.com>
2016-01-30 16:29:53 +0000

    Modified Particle2DVerlet, correcting force calc and printing files that allow for method error trapping.

--- Particle2DVerlet.java
+++ Particle2DVerlet.java
@@ -1,7 +1,7 @@
 /**
  * Computer Modelling, Exercise 3.
  *
- * Run with ARGUMENTS: output input number_of_steps timestep
+ * Run with ARGUMENTS: output input number_of_steps timestep checkF checkO
  *
  * @author M. Kirsz
  * @author R. Pruciak
@@ -20,7 +20,13 @@ public class Particle2DVerlet {
 
 	// Opens the output file
 	String outFile = argv[0];
+	String outFile2 = "checkF.dat";
+	String outFile3 = "checkO.dat";
+	String outFile4 = "totEfluct.dat";
         PrintWriter output = new PrintWriter(new FileWriter(outFile));
+	PrintWriter check1 = new PrintWriter(new FileWriter(outFile2));
+	PrintWriter check2 = new PrintWriter(new FileWriter(outFile3));
+	PrintWriter fluct = new PrintWriter(new FileWriter(outFile4));
 
 	// Count number of particles in an input file
 	BufferedReader inputFile = new BufferedReader(new FileReader(argv[1]));
@@ -41,23 +47,19 @@ public class Particle2DVerlet {
 	 * Start of the Verlet algorithm
 	 */
 
-	output.printf("%s %f\n", Particle3D.unitVec(Central,Orbital), Particle3D.magForce(Central,Orbital));
-	output.printf("%s %f\n", Particle3D.vecForce(Central,Orbital), Particle3D.vecForce(Central,Orbital).mag());
-	output.printf("%s %f\n", Particle3D.pSep(Central,Orbital), Particle3D.pSep(Central,Orbital).mag());
-	output.printf("%s %f\n", Particle3D.potEnergy(Central,Orbital), Particle3D.pSep(Central,Orbital).mag());
 	// Prints the intial position to file
-	//		output.printf("%s %s\n", Orbital.getPosition().getX(), Orbital.getPosition().getY());
-
-	// Prints initial time and total energy to file
-	// output.printf("%10.5f %10.10f\n", t, Particle3D.totEnergy(Orbital, Central));
+	output.printf("%s %s\n", Orbital.getPosition().getX(), Orbital.getPosition().getY());
 
 	// Initial force vector
-	double magForce = -Orbital.getMass()*Central.getMass() / Particle3D.pSep(Orbital, Central).magSq();
-	Vector3D force = new Vector3D(Particle3D.pSep(Orbital, Central).scalMul(magForce));
+	double magForce = Orbital.getMass()*Central.getMass() / Particle3D.pSep(Orbital, Central).magSq();
+	Vector3D force = new Vector3D(Particle3D.pSep(Orbital,Central).scalMul(magForce).scalDiv(Particle3D.pSep(Orbital,Central).mag()));
 
-	// // // // print total energy, k. energy, p. energy, force (vec + mag) on F_12 and F_21
-	// change mass to >1
-
+	// Prints checks on forces
+	// magForce(p1,p2) - force on p1 due to p2
+	check1.printf("%f %s\n", magForce, force);
+	check1.printf("%f %s\n", Particle3D.magForce(Orbital,Central), Particle3D.vecForce(Orbital,Central));
+	check1.printf("%f %s\n\n", Particle3D.magForce(Central,Orbital), Particle3D.vecForce(Central,Orbital));
+   
 	/*
 	 * Loop over timesteps
 	 */
@@ -67,8 +69,8 @@ public class Particle2DVerlet {
 	    Orbital.leapPosition(dt,force);
 
 	    // Force after time leap
-	    double magForce_new = -Orbital.getMass()*Central.getMass() / Particle3D.pSep(Orbital, Central).magSq();
-	    Vector3D force_new = new Vector3D(Particle3D.pSep(Orbital, Central).scalMul(magForce_new));
+	    double magForce_new = Orbital.getMass()*Central.getMass() / Particle3D.pSep(Orbital, Central).magSq();
+	    Vector3D force_new = new Vector3D(Particle3D.pSep(Orbital,Central).scalMul(magForce_new).scalDiv(Particle3D.pSep(Orbital,Central).mag()));
 	   
 	    // Update the velocity ready for the next position update
 	    Orbital.leapVelocity(dt, Vector3D.vecAdd(force, force_new).scalDiv(2));
@@ -82,12 +84,31 @@ public class Particle2DVerlet {
 
 	    // Prints the current position to file
 	    output.printf("%s %s\n", Orbital.getPosition().getX(), Orbital.getPosition().getY() );
+	  
 
-	    // Prints current time and total energy to file
-	    // output.printf("%10.5f %10.10f\n", t, Particle3D.totEnergy(Orbital, Central));
+	    // Prints checks on forces 
+	    check1.printf("%.5f %s\n", magForce, force);
+	    check1.printf("%f %s\n", Particle3D.magForce(Orbital,Central), Particle3D.vecForce(Orbital,Central));
+	    check1.printf("%f %s\n\n", Particle3D.magForce(Central,Orbital), Particle3D.vecForce(Central,Orbital));
+
+	    // Prints checks unitVec, pSep, kEnergy, potEnergy, totEnergy
+	    check2.printf("%f %f ", Particle3D.unitVec(Central,Orbital).mag(), Particle3D.unitVec(Orbital,Central).mag());
+	    check2.printf("%s %s ", Particle3D.unitVec(Central,Orbital), Particle3D.unitVec(Orbital,Central));
+	    check2.printf("%s %s ", Particle3D.pSep(Central,Orbital), Particle3D.pSep(Orbital,Central));
+	    check2.printf("%s %s ", Particle3D.kEnergy(Central), Particle3D.kEnergy(Orbital));
+	    check2.printf("%s %s ", Particle3D.potEnergy(Central,Orbital), Particle3D.potEnergy(Orbital,Central));
+	    check2.printf("%s %s\n\n", Particle3D.totEnergy(Orbital,Central), Particle3D.totEnergy(Central,Orbital));
+
+	    // Prints time and energy
+	    fluct.printf("%.15f %.15f\n", t,  Particle3D.totEnergy(Orbital,Central));
+
+
 	}
 
-	// Close the output file
+	// Close the output files
 	output.close();
+	check1.close();
+	check2.close();
+	fluct.close();
     }
 }
